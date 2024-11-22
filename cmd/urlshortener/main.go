7f8d8d1624564cab1ce08fbc9fb1c31563c224fc
breadlydev <breadydev@gmail.com>
2024-11-22 22:48:33 +0600

    added storage

--- cmd/urlshortener/main.go
+++ cmd/urlshortener/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"log/slog"
+	"os"
 	"urlshortener/internal/config"
+	"urlshortener/internal/lib/logger/sl"
+	"urlshortener/internal/storage/sqlite"
 	"urlshortener/internal/utils/logger"
 )
 
@@ -12,6 +15,14 @@ func main() {
 	log := logger.SetupLogger(cfg.Env)
 
 	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 
 	// TODO: init storage: sqlite
 
